92fe851ffd2f54de88f392233ca6d96b4711bcb2
Junior Alves <amdoim@gmail.com>
2025-07-06 11:12:29 -0300

    fix: login form action and custom routes

--- custom.routes.js
+++ custom.routes.js
@@ -1,21 +1,12 @@
 import { Router } from 'express'
 import index from './src/controllers/index.js'
-import { loginPage } from './src/controllers/login.js'
+import { loginPage, login } from './src/controllers/login.js'
 
 const router = Router()
 router.get('/', index)
 
 router.get('/login', loginPage)
-
-router.get('/logout', (req, res) => {
-    req.session.destroy(err => {
-        if (err) {
-            return res.redirect('/');
-        }
-        res.clearCookie('connect.sid');
-        res.redirect('/login');
-    });
-});
+router.post('/login', login)
 
 export default router
 
